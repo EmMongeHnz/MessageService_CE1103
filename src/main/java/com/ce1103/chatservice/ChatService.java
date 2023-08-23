ec9dea4ae7b6ea1038a4e78f9932a62fbd415442
EMERSON_MONGE_H <emmonge@estudiantec.cr>
2023-08-22 21:11:21 -0600

    Initial commit

---
+++ src/main/java/com/ce1103/chatservice/ChatService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ce1103.chatservice;
+
+/**
+ *
+ * @author monge
+ */
+public class ChatService {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
